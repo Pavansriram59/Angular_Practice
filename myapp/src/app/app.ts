6bb97f58d0da6cb27694ab3ef1610919028b2c44
Pavansriram <95296797+Pavansriram59@users.noreply.github.com>
2026-05-12 13:08:49 +0000

    custom directive changes, added gitignore file

--- myapp/src/app/app.ts
+++ myapp/src/app/app.ts
@@ -1,12 +1,28 @@
-import { Component, signal } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Custom } from './custom';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Custom],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  host:{
+    '(click)':'onConfirmLeavePage($event)'
+  }
 })
 export class App {
   protected readonly title = signal('myapp');
+  queryParam = 'myapp-docs-link';
+
+  onConfirmLeavePage(event: MouseEvent) {
+    const confirmation = confirm('Are you sure you want to leave this page?');
+    if (confirmation) {
+      const address = (event.target as HTMLAnchorElement).href;
+      (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParam;
+      return;
+    }
+    
+    event.preventDefault();
+  }
 }
